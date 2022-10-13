0149954194426760fad70d47653826ddfa61b903
Aman Patel <92400228+amanp8l@users.noreply.github.com>
2022-10-13 10:16:58 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -3,7 +3,7 @@
 function palindrome(str){
   var n=str.length;
   for(let i=0;i<n/2;i++){
-    if(str.charAt(i)!=str.charAt(n-i-1) && (str.charAt(i)-'a')-(str.charAt(n-i-1)-'a')!=26){
+    if(str.charAt(i)!=str.charAt(n-i-1) || (str.charAt(i)-'a')-(str.charAt(n-i-1)-'a')!=26 || (str.charAt(i)-'a')-(str.charAt(n-i-1)-'a')!=-26){
       return false;
     }
   }
