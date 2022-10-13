c2f16fb33794c109ec2af4d1863bdd2da69bcdf3
Aman Patel <92400228+amanp8l@users.noreply.github.com>
2022-10-13 10:07:16 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,12 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-//   var n=str.length;
-//   for(let i=0;i<n/2;i++){
-//     if(str.charAt(i)!=str.charAt(n-i-1)){
-//       return false;
-//     }
-//   }
+  var n=str.length;
+  for(let i=0;i<n/2;i++){
+    if(str.charAt(i)!=str.charAt(n-i-1)){
+      return false;
+    }
+  }
   return true;
 }
 module.exports = palindrome
